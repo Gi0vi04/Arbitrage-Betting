d8759ca85f83afb327d5af5a8d3206b9f89391bb
Gi0vi04 <gioviblack0904@gmail.com>
2026-04-30 21:24:05 +0200

    Improve results readability

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,39 +1,33 @@
 def print_result(result):
     event = result["event"]
-
     perc = result["perc"]
-    
+
     home_bookmaker = result["home_bookmaker"]
     away_bookmaker = result["away_bookmaker"]
-    
+
     home_price = result["home_price"]
     away_price = result["away_price"]
 
-    output = []
-    output.append("\n" + "=" * 60)
-    output.append(event)
-    output.append("-" * 60)
-    output.append(f"Profit teorico: {perc:.4f}")
+    return f"""
+EVENT: {event}
 
-    output.append(f"\nBET 1:")
-    output.append(f"Bookmaker: {home_bookmaker}")
-    output.append(f"Risultato: 1")
-    output.append(f"Quota: {home_price}")
-    output.append(f"Percentuale puntata: {(1/home_price * 100):.2f}%")
+Profit: {perc:.4f}
 
-    output.append(f"\nBET 2:")
-    output.append(f"Bookmaker: {away_bookmaker}")
-    output.append(f"Risultato: 2")
-    output.append(f"Quota: {away_price}")
-    output.append(f"Percentuale puntata: {(1/away_price * 100):.2f}%")
+── BET 1 ─────────────────────
+Bookmaker        : {home_bookmaker}
+Market           : 1 (Home)
+Odds             : {home_price}
+Stake percentage : {(1/home_price * 100):.2f}%
 
-    output.append("=" * 60)
+── BET 2 ─────────────────────
+Bookmaker        : {away_bookmaker}
+Market           : 2 (Away)
+Odds             : {away_price}
+Stake percentage : {(1/away_price * 100):.2f}%
 
-    final_output = "\n".join(output)
-    
-    return final_output
+"""
 
 def save_results(results):
-    with open("results.txt", "w") as f:
+    with open("results.txt", "w", encoding="utf-8") as f:
         for result in results:
             f.write(print_result(result) + "\n")
